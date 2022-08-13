fac80263e65174558893e24b7188ac47fba8af98
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-08-13 14:24:43 +0530

    single food item route added

--- food_items/src/App.jsx
+++ food_items/src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
 import FoodItems from "./Components/FoodItems/FoodItems";
-
+import SingleFoodItem from "./Components/SingleFoodItem/SingleFoodItem";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<FoodItems/>}></Route>
+        <Route path="/" element={<FoodItems />}></Route>
+        <Route path="/:code" element={<SingleFoodItem/>}></Route>
       </Routes>
     </div>
   );
---
+++ food_items/src/Components/SingleFoodItem/SingleFoodItem.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "../SingleFoodItem/SingleFoodItem.css";
+import Navbar from "../Navbar/Navbar";
+import FoodItems from "../FoodItems/FoodItems";
+import { useState } from "react";
+import { useParams } from "react-router";
+import { useEffect } from "react";
+import axios from "axios";
+function SingleFoodItem() {
+  let [fooditem, setFooditem] = useState({});
+  let { code } = useParams();
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/Items").then((res) => {
+      let item = res.data.filter((e) => e.code == code);
+      setFooditem(...item);
+    });
+  }, []);
+  return (
+    <div  className="parent">
+      <Navbar />
+      <div className="child">
+        <img
+          src="https://mathworld.wolfram.com/images/eps-svg/TetrahedronProj1_500.svg"
+          alt=""
+          className="image"
+        />
+        <p className="food">{fooditem.product_name}</p>
+      </div>
+      <div classname="child1">
+        <table border={{}}>
+          <tbody>
+            {Object.keys(fooditem).map((key, index) => {
+              return (
+                <tr key={index}>
+                      <td style={{fontWeight:"bold"}} >{key}</td>
+                  <td>{fooditem[key]}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default SingleFoodItem;
